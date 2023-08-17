0a2ce30b08735a6a9e03b2b615440ad393c4de21
Bing Guo <gbinux@gmail.com>
2023-08-17 12:09:12 -0400

    add --minmac cli arguments for call_hmmibd

--- bin/call_hmmibd.py
+++ bin/call_hmmibd.py
@@ -18,6 +18,7 @@ def get_args():
     parser.add_argument("--n", type=int, default=100)
     parser.add_argument("--m", type=int, default=5)
     parser.add_argument("--chrno", type=int, required=True)
+    parser.add_argument("--minmac", type=int, default=1, help="min MAC")
     parser.add_argument("--mincm", type=float, default=2.0)
     parser.add_argument("--genome_set_id", type=int, required=True)
 
@@ -31,6 +32,7 @@ seqlen_in_cm = args.seqlen / bp_per_cm
 chrno = args.chrno
 n = args.n
 m = args.m
+minmac = args.minmac
 mincm = args.mincm
 
 # ------------------- make genetic map ----------------------------
@@ -44,6 +46,7 @@ with open(f"{chrno}.map", "w") as f:
 calldata = allel.read_vcf(vcf, fields=["samples", "CHROM", "POS", "GT"])
 samples = calldata["samples"]
 gt = calldata["calldata/GT"][:, :, 0]
+minmac = minmac // 2  # given GT is using ploidy 0 only in the above line
 
 df_pos = pd.DataFrame(
     {
@@ -52,6 +55,17 @@ df_pos = pd.DataFrame(
     }
 )
 df_gt = pd.DataFrame(gt, columns=samples)
+
+# filter site by minmac
+nsample = df_gt.shape[1]
+ac = df_gt.sum(axis=1).to_numpy()
+# .  get mac from ac
+sel = ac * 2 > nsample
+ac[sel] = nsample - ac[sel]
+sel = ac >= minmac
+df_pos = df_pos.loc[sel, :]
+df_gt = df_gt.loc[sel, :]
+
 
 df_hmm_inputs = pd.concat([df_pos, df_gt], axis=1)
 df_hmm_inputs.to_csv("hmm_inputs.txt", sep="\t", index=None)
