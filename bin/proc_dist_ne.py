5fed60008feba103abe43266237bb1c16790a3ce
Bing Guo <gbinux@gmail.com>
2024-04-20 18:05:32 -0400

    add peak_validate_meth, ibdne_no_diploid_conversion option

--- bin/proc_dist_ne.py
+++ bin/proc_dist_ne.py
@@ -15,10 +15,26 @@ pa.add_argument("--ibd_files_true", type=str, nargs=14, default=[])
 pa.add_argument("--vcf_files", type=str, nargs=14, required=True)
 pa.add_argument("--genome_set_id", type=str, required=True)
 pa.add_argument("--ibdne_jar", type=str, default=ibd_jar_default)
+# TODO: add the option to nextflow process and set a the default in params of nextflow
+pa.add_argument(
+    "--peak_validate_meth", type=str, choices=["xirs", "ihs"], default="xirs"
+)
 pa.add_argument("--ibdne_mincm", type=float, default=2)
 pa.add_argument("--ibdne_minregion", type=float, default=10)
 pa.add_argument("--ibdne_flatmeth", type=str, default="none")
+# This can be used to specify which chromosome to use, especially when selection
+# is not established on some chromosomes and we only use those that are
+# established to better observe selection effects
+pa.add_argument("--ibdne_chrlist", type=int, nargs="+", default=None)
+pa.add_argument(
+    "--ibdne_no_diploid_conversion",
+    type=str,
+    choices=["true", "false"],
+    default="false",
+)
 args = pa.parse_args()
+
+print(args)
 
 assert args.ibdne_jar is not None
 assert args.ibdne_flatmeth in ["none", "merge", "keep_hap_1_only"]
@@ -94,30 +110,57 @@ ibd.filter_ibd_by_time(min_tmrca=1.5)
 ibd.filter_ibd_by_length(min_seg_cm=ibdne_mincm)
 
 # calculate XiR,s
-xirs_df = ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+if args.peak_validate_meth == "xirs":
+    ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+elif args.peak_validate_meth == "ihs":
+    ibd.calc_ihs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
 
-# convert to heterzygous diploids
-# Note: remove_hbd might not remove a lot segments as
-# hdb only involves n/2 pairs of a total of n(n-1)/2 pairs (ratio: 1/(n-1)).
-# When n is large, the difference is small
-ibd.convert_to_heterozygous_diploids(remove_hbd=True)
 
-if args.ibdne_flatmeth != "none":
-    ibd.flatten_diploid_ibd(method=args.ibdne_flatmeth)
+if args.ibdne_no_diploid_conversion == "true":
+    print("skip diploid conversion")
+elif args.ibdne_no_diploid_conversion == "false":
+    # convert to heterzygous diploids
+    # Note: remove_hbd might not remove a lot segments as
+    # hdb only involves n/2 pairs of a total of n(n-1)/2 pairs (ratio: 1/(n-1)).
+    # When n is large, the difference is small
+    ibd.convert_to_heterozygous_diploids(remove_hbd=True)
+
+    if args.ibdne_flatmeth != "none":
+        ibd.flatten_diploid_ibd(method=args.ibdne_flatmeth)
+else:
+    raise NotImplementedError(
+        f"{args.ibdne_no_diploid_conversion} is not valid value is not a value value for option --ibdne_no_diploid_conversion"
+    )
 
 # calculate coverage and remove peaks
 ibd.calc_ibd_cov()
 ibd.find_peaks()
 
-ibd.filter_peaks_by_xirs(xirs_df)
 
-
-of_orig_ibdne_obj = f"{label_str}_orig.ibdne.ibdobj.gz"
-ibd.pickle_dump(of_orig_ibdne_obj)
+if args.peak_validate_meth == "xirs":
+    xirs_df = ibd._xirs_df
+    ibd.filter_peaks_by_xirs(xirs_df)
+elif args.peak_validate_meth == "ihs":
+    ibd.filter_peaks_by_ihs(min_ihs_hits=1)
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
 
 ibd2 = ibd.duplicate(f"{label_str}_rmpeaks")
 ibd2.remove_peaks()
 ibd2._df = ibd2.cut_and_split_ibd()
+
+
+# filter by chromosome for Ne
+if args.ibdne_chrlist is not None:
+    ibd._df = ibd._df[lambda df: df.Chromosome.isin(args.ibdne_chrlist)]
+    ibd2._df = ibd2._df[lambda df: df.Chromosome.isin(args.ibdne_chrlist)]
+
+# write ne file
+of_orig_ibdne_obj = f"{label_str}_orig.ibdne.ibdobj.gz"
+ibd.pickle_dump(of_orig_ibdne_obj)
+
 of_rmpeaks_ibdne_obj = f"{label_str}_rmpeaks.ibdne.ibdobj.gz"
 ibd2.pickle_dump(of_rmpeaks_ibdne_obj)
 
--- bin/proc_infomap.py
+++ bin/proc_infomap.py
@@ -1,19 +1,38 @@
 #! /usr/bin/env python3
 
-import ibdutils.utils.ibdutils as ibdutils
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
+import ibdutils.utils.ibdutils as ibdutils
 
 # parse arguments
 pa = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-pa.add_argument("--ibd_files", type=str, nargs=14, required=True)
-pa.add_argument("--vcf_files", type=str, nargs=14, required=True)
+pa.add_argument("--ibd_files", type=str, nargs="+", required=True)
+pa.add_argument("--vcf_files", type=str, nargs="+", required=True)
 pa.add_argument("--genome_set_id", type=int, required=True)
+
+# TODO: update nextflow main pipeline so the process has the this option and params has a default value
+pa.add_argument(
+    "--peak_validate_meth", type=str, choices=["xirs", "ihs"], default="xirs"
+)
 args = pa.parse_args()
 
+# use the number files to indicate the number of chromosome included
+# in the genome_set
+nchrom = len(args.ibd_files)
+assert nchrom == len(args.vcf_files), "ibd /vcf file lists should be of the same length"
+
+# genome
+if nchrom == 1:
+    genome = ibdutils.Genome.get_genome_simple_simu(
+        r=0.01 / 15000, nchroms=1, seqlen_bp_chr=100 * 15000
+    )
+elif nchrom == 14:
+    genome = ibdutils.Genome.get_genome("simu_14chr_100cm")
+else:
+    raise NotImplemented("nchrom can only be 1 or 14")
 
 # read ibd
-genome_14_100 = ibdutils.Genome.get_genome("simu_14chr_100cm")
-ibd = ibdutils.IBD(genome=genome_14_100, label="orig")
+ibd = ibdutils.IBD(genome=genome, label="orig")
 ibd.read_ibd(ibd_fn_lst=args.ibd_files)
 
 
@@ -33,8 +52,14 @@ ibd.calc_ibd_cov()
 ibd.find_peaks()
 
 # calculate XiR,s and filter peaks
-xirs_df = ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
-ibd.filter_peaks_by_xirs(xirs_df)
+if args.peak_validate_meth == "xirs":
+    xirs_df = ibd.calc_xirs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+    ibd.filter_peaks_by_xirs(xirs_df)
+elif args.peak_validate_meth == "ihs":
+    ibd.calc_ihs(vcf_fn_lst=args.vcf_files, min_maf=0.01)
+    ibd.filter_peaks_by_ihs(min_ihs_hits=1)
+else:
+    raise NotImplementedError(f"{args.peak_validate_meth} is not valid method")
 
 
 ibd2 = ibd.duplicate("rmpeak")
