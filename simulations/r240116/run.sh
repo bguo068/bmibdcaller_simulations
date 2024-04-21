c92245399726b859beef88fef65081b30cbd3a03
Bing Guo <gbinux@gmail.com>
2024-04-20 22:12:45 -0400

    add notes/scripts for r240116

---
+++ simulations/r240116/run.sh
@@ -0,0 +1,20 @@
+
+. ~/conda_devel.sh
+conda activate bmibdcaller_simulations
+mkdir -p /local/scratch/bing/bmibdcaller_simulations/simulations/r240116/
+cd /local/scratch/bing/bmibdcaller_simulations/simulations/r240116/
+
+# NOTE: to accelerate the process
+# 1. use profile hq_recomb where isorelate/hmmibd were assigned more resources
+# 2. use the hmmibd-rs version to allow multithreading
+~/.local/bin/nextflow /local/chib/toconnor_grp/bing/bmibdcaller_simulations/main2.nf  \
+    -resume \
+    -entry WF_VARY_RECOM_RATE \
+    -profile hq_recomb \
+    --hmmibd_version hmmibd2
+
+# test with -stub option
+# ~/.local/bin/nextflow /local/chib/toconnor_grp/bing/bmibdcaller_simulations/main2.nf  \
+#     -entry WF_VARY_RECOM_RATE \
+#     -stub \
+#     --hmmibd_version hmmibd2
