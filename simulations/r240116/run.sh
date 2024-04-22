e5830fd198a0e53de9d234ca61bfdc011b58b057
Bing Guo <gbinux@gmail.com>
2024-04-22 18:05:15 -0400

    use new main.nf after use main2.nf to overwrite old main.nf

--- simulations/r240116/run.sh
+++ simulations/r240116/run.sh
@@ -7,14 +7,14 @@ cd /local/scratch/bing/bmibdcaller_simulations/simulations/r240116/
 # NOTE: to accelerate the process
 # 1. use profile hq_recomb where isorelate/hmmibd were assigned more resources
 # 2. use the hmmibd-rs version to allow multithreading
-~/.local/bin/nextflow /local/chib/toconnor_grp/bing/bmibdcaller_simulations/main2.nf  \
+~/.local/bin/nextflow /local/chib/toconnor_grp/bing/bmibdcaller_simulations/main.nf  \
     -resume \
     -entry WF_VARY_RECOM_RATE \
     -profile hq_recomb \
     --hmmibd_version hmmibd2
 
 # test with -stub option
-# ~/.local/bin/nextflow /local/chib/toconnor_grp/bing/bmibdcaller_simulations/main2.nf  \
+# ~/.local/bin/nextflow /local/chib/toconnor_grp/bing/bmibdcaller_simulations/main.nf  \
 #     -entry WF_VARY_RECOM_RATE \
 #     -stub \
 #     --hmmibd_version hmmibd2
